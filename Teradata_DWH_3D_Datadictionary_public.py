ef668f94e78b8ff1aaff25f7e570588b66875c59
Marco Genca <54363103+Marco334@users.noreply.github.com>
2020-09-22 02:15:01 +0200

    Update Teradata_DWH_3D_Datadictionary_public.py
    MULTI Database management upgrade

--- Teradata_DWH_3D_Datadictionary_public.py
+++ Teradata_DWH_3D_Datadictionary_public.py
@@ -1,12 +1,20 @@
- import teradata
+import teradata
 import teradatasql
 import pandas as pd
 import bpy
 import math
 #RUN BLENDER AS ADMINISTRATOR !!!!
+#MODIFY TABLE DICTIONARY IN DB_LIST
 #pip install teradatasql
+#SELECT * FROM DBC.STATSV WHERE STATSID = 0;
 
-# SELECT * FROM DBC.STATSV WHERE STATSID = 0; 
+#TO DO:
+# pop-up DB
+# pop-up connection
+# Gestione multiple DB      # IN PROGRESS
+# Statistic allert blinking
+# Auto background setup
+
 def DB_CONNECTION_MNG_1():
    print ( '\n INIZIO CONNESSIONE \n' )
    host,username,password = 'xxx.xxx.xxx.xxx','xxxxx', 'xxxx'
@@ -16,7 +24,7 @@ def DB_CONNECTION_MNG_1():
    return(udaExec.connect(method="odbc", system=host,username="dbc", password="dbc"))
 
 def Collection_MNG_1():
-#--------- COLLECTION MANAGEMENT 
+#--------- COLLECTION MANAGEMENT
    collection_T = bpy.data.collections.new('TABLES')
    bpy.context.scene.collection.children.link(collection_T)
    collection_V = bpy.data.collections.new('VIEW')
@@ -36,145 +44,155 @@ def Color_MNG_1():
     ROSSO = bpy.data.materials.new(name="Red_S" ) #allert cube for old statistics
     ROSSO.diffuse_color = (1, 0, 0,0)
     return(1)
-    
+
 def Color_MNG_2():
    #The four values are represented as: [Red, Green, Blue, Alpha]
    ORAN  = bpy.data.materials.new(name="Orange_T"  ) #set new material to variable
-   ORAN.diffuse_color = (0.9,0.7,0.01,0) 
+   ORAN.diffuse_color = (0.9,0.7,0.01,0)
    GLASS = bpy.data.materials.new(name="Glass_V" ) #set new material to variable
-   GLASS.diffuse_color = (0.1, 0.5, 0.7,0) 
+   GLASS.diffuse_color = (0.1, 0.5, 0.7,0)
    return(1)
-    
-  
+
+def Auto_DEL_ALL_OBJ():
+   scene = bpy.context.scene
+   for object in scene.objects:
+    bpy.ops.object.delete()
+
 Color_d_FL  = 0
 DBCONN_d_FL = 0
-session = DB_CONNECTION_MNG_1()
-             
-#query =" SELECT 'SELECT   '''||TRIM(DATABASENAME)|| '.' || TRIM(TABLENAME) || ''' '   ||  ' AS TABLE_NAME,COUNT(*) AS ROW_COUNT FROM ' || TRIM(DATABASENAME) || '.' || TRIM (TABLENAME) || ' '  FROM DBC.TABLES WHERE TABLEKIND IN ('T','V')  AND DATABASENAME = 'TEST_DB_PAYROLL';  "  
-query =" SELECT 'SELECT   '''||TRIM(DATABASENAME)|| '.' || TRIM(TABLENAME) || ''' '   ||  ' AS TABLE_NAME, '''||TRIM(TABLEKIND)|| ''' '   ||  ' AS TABLE_KIND,COUNT(*) AS ROW_COUNT FROM ' || TRIM(DATABASENAME) || '.' || TRIM (TABLENAME) || ' '  FROM DBC.TABLES WHERE TABLEKIND IN ('T','V')  AND DATABASENAME = 'TEST_DB_PAYROLL' AND TABLENAME<>'TEMP_DB_INFO_V';"  
-
-QUERY_2 = [] 
-print('\n PREPARING QUERY \n') 
-curr_1 =  session.execute(query)
-sql_subq = pd.DataFrame(curr_1.fetchall())
-sql_subq['UNION']='UNION ALL'
-sql_subq1 = sql_subq[sql_subq.columns].astype(str).apply(lambda x: ' '.join(x), axis = 1)
-sql_subq1 =  ' '.join(sql_subq1)
-#print(sql_subq1)
-
-QUERY_2 = str(sql_subq1)[:int(-9) ]
-QUERY_2 = str('REPLACE VIEW TEST_DB_PAYROLL.TEMP_DB_INFO_V AS (\n')+ QUERY_2 + str(' );\n')  #str('ORDER BY 3 ASC ;\n') ##TTRR
-#print(QUERY_2)
-#for row in session.execute(query):
-#   #print(row) 
-#   if ciclo != 0:
-#        QUERY_2.append(str(' \n UNION ALL \n') )
-#     
-#   QUERY_2.append(str(row)[int(8):int(-1) ])
-#   ciclo = +1
-
-QUERY_3 = ''.join( QUERY_2 )
-#print(QUERY_3)
+OBJ_HGT     = 50
+DB_FLR      = OBJ_HGT/2 # 25 first floor location - it have to be increased every loop at least of  OBJ_HGT +3
  
-print('\n EXECUTING QUERY \n') 
-session.execute(QUERY_3)
+Auto_DEL_ALL_OBJ() #Delete preexisting objects
 
-QUERY_4 = str("SELECT DD.TABLE_NAME , TABLE_KIND,ROW_COUNT,LAST_COLLECT_S , FF.ACTUALSPACE FROM TEST_DB_PAYROLL.TEMP_DB_INFO_V as DD LEFT OUTER JOIN (SELECT DatabaseName ||'.'|| TableName AS TABLE_NAME ,MAX( LastCollectTimeStamp) AS LAST_COLLECT_S FROM DBC.STATSV WHERE StatsId = 0 AND DatabaseName = 'TEST_DB_PAYROLL'      group by 1 ) as ee      ON ee.TABLE_NAME  = DD.TABLE_NAME      LEFT OUTER JOIN (SELECT trim(DatabaseName) ||'.'|| trim(TableName) AS TABLE_NAME           ,SUM(CURRENTPERM)/(1024) AS ACTUALSPACE FROM DBC.TABLESIZE WHERE DATABASENAME = 'TEST_DB_PAYROLL' group by 1 ) AS FF ON FF.TABLE_NAME  = DD.TABLE_NAME order by 3 ;")
-curr_2 = session.execute(QUERY_4)
-sql_data = pd.DataFrame(curr_2.fetchall())
-#print(sql_data)
-#print('\n\n\n')
-sql_data = sql_data.rename(columns={0: 'TABLE_NAME',1:'OBJECT_KIND',2: 'ROW_COUNT'})
-print(sql_data)     
-
-
-#--------- COLLECTION MANAGEMENT 
-Collection_MNG_1() 
-  
-#--------- COLOR MANAGEMENT 
+#--------- COLLECTION MANAGEMENT
+Collection_MNG_1()
+#--------- COLOR MANAGEMENT
 #The four values are represented as: [Red, Green, Blue, Alpha]
-Color_d_FL = Color_MNG_1() 
-
-
-CICLO_0 = 5
-CICLO_1 = 0
-CICLO_V = -5
-for index, row in sql_data.iterrows():
- #OBJ_N = str(row[0])
- #ROW_C = int(row[2])
- #OBJ_T = str(row[1])
- #STT_D = str(row[3])
- OBJ_N = str(row['TABLE_NAME'])
- ROW_C = int(row['ROW_COUNT'])
- OBJ_T = str(row['OBJECT_KIND'])
- STT_D = row[3].date()
- NOW_V = pd.to_datetime("now").date()
- MEM_D = float( row[4] if row[4] is not None else -90 )
- print(OBJ_N)
- print(ROW_C)
- print(OBJ_T)
- print(STT_D)
- print(MEM_D)
- print(NOW_V)
- #bpy.ops.mesh.primitive_cube_add( size = 1, location=( 0 ,CICLO_0, 25 ) )
- #bpy.ops.transform.resize(value=( ROW_C/100 , MEM_D/100 , 50) )
- #cube  = bpy.context.selected_objects[0]
- #bpy.context.active_object.name = str(OBJ_N)
- #obj = bpy.context.active_object
- #CICLO_0  = CICLO_0  +( MEM_D/2) 
- #CICLO_1  = CICLO_1  +( ROW_C/100 ) 
- if OBJ_T == 'T': # if the object is a Table 
-  CICLO_0  = CICLO_0+( MEM_D/20) 
-  bpy.ops.mesh.primitive_cube_add( size = 1, location=( ROW_C/100 ,CICLO_0, 25 ) )
-  bpy.ops.transform.resize(value=( ROW_C/50 , MEM_D/10 , 50) )
-  cube  = bpy.context.selected_objects[0]
-  bpy.context.active_object.name = str(OBJ_N)
-  obj = bpy.context.active_object
-  #CICLO_1  = CICLO_1  +( ROW_C/100 )  
-  bpy.data.collections['TABLES'].objects.link(obj)
-  #bpy.data.collections['TABLES.001'].objects.link(obj)
-  bpy.context.scene.collection.objects.unlink(obj)
-  bpy.context.active_object.data.materials.append(ORAN)#add the material to the object
-  #-----------------------------------------------------------------
-  font_curve = bpy.data.curves.new(type="FONT",name=OBJ_N)
-  font_curve.body = "TEBLE :"+OBJ_N
-  font_obj = bpy.data.objects.new("TEXT_" + OBJ_N, font_curve)
-  #bpy.ops.transform.resize(value=( ROW_C/50 , MEM_D/10 , 50) )
-  font_obj.location = (1 , CICLO_0-MEM_D/20, 51)
-  bpy.data.collections['TEXT'].objects.link(font_obj)
-  #bpy.context.scene.collection.objects.unlink(font_obj) 
-  #-----------------------------ALLERT OLD STATISTIC ------------------------------------
-  if STT_D < (NOW_V + pd.DateOffset(days=7)).date():
-   bpy.ops.mesh.primitive_cube_add( size = 2, location=(-1,(CICLO_0-MEM_D/20)+1, 49) ) 
+#Color_d_FL = Color_MNG_1()
+ORAN  = bpy.data.materials.new(name="Orange_T"  ) #TEBLE - set new material to variable
+ORAN.diffuse_color = (0.98,0.225,0.01,1)
+GLASS = bpy.data.materials.new(name="Glass_V" ) #VIEW - set new material to variable
+GLASS.diffuse_color = (0.1, 0.5, 0.7,0)
+ROSSO = bpy.data.materials.new(name="Red_S" ) #allert cube for old statistics
+ROSSO.diffuse_color = (1, 0, 0,0)
+#--------- GESTIONE DB MULTIPLI
+DB_LIST = {0 : 'TEST_DB_PAYROLL',1 :'TEST_DB_PAYROLLS_2'}
+DB_CNT  = len(DB_LIST)
+#--------- CONNECTION
+session = DB_CONNECTION_MNG_1()
+for i in DB_LIST:
+ DB_NAME = DB_LIST[i]
+ QUERY = str(" SELECT 'SELECT   '''||TRIM(DATABASENAME)|| '.' || TRIM(TABLENAME) || ''' '   ||  ' AS TABLE_NAME, '''||TRIM(TABLEKIND)|| ''' '   ||  ' AS TABLE_KIND,COUNT(*) AS ROW_COUNT FROM ' || TRIM(DATABASENAME) || '.' || TRIM (TABLENAME) || ' '  FROM DBC.TABLES WHERE TABLEKIND IN ('T','V')  AND DATABASENAME = '")+ DB_NAME + str("' AND TABLENAME<>'TEMP_DB_INFO_V';")
+ print(DB_NAME)
+ #print(QUERY)
+ #QUERY =" SELECT 'SELECT   '''||TRIM(DATABASENAME)|| '.' || TRIM(TABLENAME) || ''' '   ||  ' AS TABLE_NAME, '''||TRIM(TABLEKIND)|| ''' '   ||  ' AS TABLE_KIND,COUNT(*) AS ROW_COUNT FROM ' || TRIM(DATABASENAME) || '.' || TRIM (TABLENAME) || ' '  FROM DBC.TABLES WHERE TABLEKIND IN ('T','V')  AND DATABASENAME = 'TEST_DB_PAYROLL' AND TABLENAME<>'TEMP_DB_INFO_V';"
+ #QUERY  = str(" SELECT 'SELECT   '''||TRIM(DATABASENAME)|| '.' || TRIM(TABLENAME) || ''' '   ||  ' AS TABLE_NAME, '''||TRIM(TABLEKIND)|| ''' '   ||  ' AS TABLE_KIND,COUNT(*) AS ROW_COUNT FROM ' || TRIM(DATABASENAME) || '.' || TRIM (TABLENAME) || ' '  FROM DBC.TABLES WHERE TABLEKIND IN ('T','V')  AND DATABASENAME = '")+ DB_NAME + str("' AND TABLENAME<>'TEMP_DB_INFO_V';")
+ QUERY_2 = []
+ print('PREPARING QUERY \n')
+ curr_1 =  session.execute(QUERY)
+ sql_subq = pd.DataFrame(curr_1.fetchall())
+ sql_subq['UNION']='UNION ALL'
+ sql_subq1 = sql_subq[sql_subq.columns].astype(str).apply(lambda x: ' '.join(x), axis = 1)
+ sql_subq1 =  ' '.join(sql_subq1)
+ #print(sql_subq1)
+ QUERY_2 = str(sql_subq1)[:int(-9) ]
+ QUERY_2 = str('REPLACE VIEW ')+ DB_NAME + str('.TEMP_DB_INFO_V AS (\n')+ QUERY_2 + str(' );\n')  #str('ORDER BY 3 ASC ;\n') ##TTRR
+ QUERY_3 = ''.join( QUERY_2 )
+ #print(QUERY_3)
+ print('EXECUTING QUERY \n')
+ session.execute(QUERY_3)
+ #QUERY_4 = str("SELECT DD.TABLE_NAME , TABLE_KIND,ROW_COUNT,LAST_COLLECT_S , FF.ACTUALSPACE FROM TEST_DB_PAYROLL.TEMP_DB_INFO_V as DD LEFT OUTER JOIN (SELECT DatabaseName ||'.'|| TableName AS TABLE_NAME ,MAX( LastCollectTimeStamp) AS LAST_COLLECT_S FROM DBC.STATSV WHERE StatsId = 0 AND DatabaseName = 'TEST_DB_PAYROLL'      group by 1 ) as ee      ON ee.TABLE_NAME  = DD.TABLE_NAME      LEFT OUTER JOIN (SELECT trim(DatabaseName) ||'.'|| trim(TableName) AS TABLE_NAME           ,SUM(CURRENTPERM)/(1024) AS ACTUALSPACE FROM DBC.TABLESIZE WHERE DATABASENAME = 'TEST_DB_PAYROLL' group by 1 ) AS FF ON FF.TABLE_NAME  = DD.TABLE_NAME order by 3 ;")
+ QUERY_4 = str("SELECT DD.TABLE_NAME , TABLE_KIND,ROW_COUNT,LAST_COLLECT_S , FF.ACTUALSPACE FROM ")+ DB_NAME + str(".TEMP_DB_INFO_V as DD LEFT OUTER JOIN (SELECT DatabaseName ||'.'|| TableName AS TABLE_NAME ,MAX( LastCollectTimeStamp) AS LAST_COLLECT_S FROM DBC.STATSV WHERE StatsId = 0 AND DatabaseName = '")+ DB_NAME + str("'      group by 1 ) as ee      ON ee.TABLE_NAME  = DD.TABLE_NAME      LEFT OUTER JOIN (SELECT trim(DatabaseName) ||'.'|| trim(TableName) AS TABLE_NAME ,SUM(CURRENTPERM)/(1024) AS ACTUALSPACE FROM DBC.TABLESIZE WHERE DATABASENAME = '")+ DB_NAME + str("' group by 1 ) AS FF ON FF.TABLE_NAME  = DD.TABLE_NAME order by 3 ;")
+ curr_2 = session.execute(QUERY_4)
+ sql_data = pd.DataFrame(curr_2.fetchall())
+ #print(sql_data)
+ sql_data = sql_data.rename(columns={0: 'TABLE_NAME',1:'OBJECT_KIND',2: 'ROW_COUNT'})
+ print(sql_data)
+ #--------- PREPARING FOR CYCLE
+ CICLO_0 = 5
+ CICLO_1 = 0
+ CICLO_V = -5
+ for index, row in sql_data.iterrows():
+  #OBJ_N = str(row[0])
+  #ROW_C = int(row[2])
+  #OBJ_T = str(row[1])
+  #STT_D = str(row[3])
+  OBJ_N = str(row['TABLE_NAME'])
+  ROW_C = int(row['ROW_COUNT'])
+  OBJ_T = str(row['OBJECT_KIND'])
+  STT_D = row[3].date()
+  NOW_V = pd.to_datetime("now").date()
+  MEM_D = float( row[4] if row[4] is not None else -90 )
+  #print(OBJ_N)
+  #print(ROW_C)
+  #print(OBJ_T)
+  #print(STT_D)
+  #print(MEM_D)
+  #print(NOW_V)
+  #bpy.ops.mesh.primitive_cube_add( size = 1, location=( 0 ,CICLO_0, DB_FLR ) )
+  #bpy.ops.transform.resize(value=( ROW_C/100 , MEM_D/100 , OBJ_HGT) )
+  #cube  = bpy.context.selected_objects[0]
+  #bpy.context.active_object.name = str(OBJ_N)
+  #obj = bpy.context.active_object
+  #CICLO_0  = CICLO_0  +( MEM_D/2)
+  #CICLO_1  = CICLO_1  +( ROW_C/100 )
+  if OBJ_T == 'T': # if the object is a Table
+   CICLO_0  = CICLO_0+( MEM_D/20)
+   bpy.ops.mesh.primitive_cube_add( size = 1, location=( ROW_C/100 ,CICLO_0, DB_FLR ) )
+   bpy.ops.transform.resize(value=( ROW_C/OBJ_HGT , MEM_D/10 , OBJ_HGT) )
    cube  = bpy.context.selected_objects[0]
-   bpy.context.active_object.name = str(OBJ_N + "Allert")
-   obj = bpy.context.active_object 
-   bpy.data.collections['STAT_ALLERT'].objects.link(obj)
-   #bpy.data.collections[''STAT_ALLERT'.001'].objects.link(obj)
+   bpy.context.active_object.name = str(OBJ_N)
+   obj = bpy.context.active_object
+   #CICLO_1  = CICLO_1  +( ROW_C/100 )
+   bpy.data.collections['TABLES'].objects.link(obj)
+   #bpy.data.collections['TABLES.001'].objects.link(obj)
    bpy.context.scene.collection.objects.unlink(obj)
-   bpy.context.active_object.data.materials.append(ROSSO)#add the material to the object 
-  #-----------------------------------------------------------------
-  CICLO_0  = CICLO_0+( MEM_D/20)+2  
-  #-----------------------------------------------------------------
- if OBJ_T == 'V': # if the object is a View 
-  bpy.ops.mesh.primitive_cube_add( size = 1, location=( ROW_C/100 ,CICLO_V, 25 ) )
-  bpy.ops.transform.resize(value=( ROW_C/50 , MEM_D/100 , 50) )
-  cube  = bpy.context.selected_objects[0]
-  bpy.context.active_object.name = str(OBJ_N)
-  obj = bpy.context.active_object 
-  bpy.data.collections['VIEW'].objects.link(obj)
-  #bpy.data.collections['VIEW.001'].objects.link(obj)
-  bpy.context.scene.collection.objects.unlink(obj)
-  bpy.context.active_object.data.materials.append(GLASS) 
-  #-----------------------------------------------------------------
-  font_curve = bpy.data.curves.new(type="FONT",name=OBJ_N)
-  font_curve.body = "VIEW :"+OBJ_N
-  font_obj = bpy.data.objects.new("TEXT_" + OBJ_N, font_curve)
-  #bpy.ops.transform.resize(value=( ROW_C/50 , MEM_D/10 , 50) )
-  font_obj.location = (1 , CICLO_V, 51)
-  bpy.data.collections['TEXT'].objects.link(font_obj)
-  #bpy.context.scene.collection.objects.unlink(font_obj)
-  CICLO_V = CICLO_V -10  
-
-QUERY_5 = str("DROP VIEW TEST_DB_PAYROLL.TEMP_DB_INFO_V;")
-session.execute(QUERY_5)
+   bpy.context.active_object.data.materials.append(ORAN)#add the material to the object
+   #-----------------------------------------------------------------
+   font_curve = bpy.data.curves.new(type="FONT",name=OBJ_N)
+   font_curve.body = "TEBLE :"+OBJ_N
+   font_obj = bpy.data.objects.new("TEXT_" + OBJ_N, font_curve)
+   #bpy.ops.transform.resize(value=( ROW_C/50 , MEM_D/10 , 50) )
+   font_obj.location = (1 , CICLO_0-MEM_D/20, OBJ_HGT+1)
+   bpy.data.collections['TEXT'].objects.link(font_obj)
+   #bpy.context.scene.collection.objects.unlink(font_obj)
+   #-----------------------------ALLERT OLD STATISTIC ------------------------------------
+   if STT_D < (NOW_V + pd.DateOffset(days=7)).date():
+    bpy.ops.mesh.primitive_cube_add( size = 2, location=(-1,(CICLO_0-MEM_D/20)+1, OBJ_HGT-1) )
+    cube  = bpy.context.selected_objects[0]
+    bpy.context.active_object.name = str(OBJ_N + "Allert")
+    obj = bpy.context.active_object
+    bpy.data.collections['STAT_ALLERT'].objects.link(obj)
+    #bpy.data.collections[''STAT_ALLERT'.001'].objects.link(obj)
+    bpy.context.scene.collection.objects.unlink(obj)
+    bpy.context.active_object.data.materials.append(ROSSO)#add the material to the object
+   #--------------------------------------------------------------------------------------
+   CICLO_0  = CICLO_0+( MEM_D/20)+2
+   #---------------------------- VIEW MANAGEMENT -----------------------------------------
+  if OBJ_T == 'V': # if the object is a View
+   bpy.ops.mesh.primitive_cube_add( size = 1, location=( ROW_C/100 ,CICLO_V, DB_FLR ) )
+   bpy.ops.transform.resize(value=( ROW_C/OBJ_HGT , MEM_D/100 , OBJ_HGT) )
+   cube  = bpy.context.selected_objects[0]
+   bpy.context.active_object.name = str(OBJ_N)
+   obj = bpy.context.active_object
+   bpy.data.collections['VIEW'].objects.link(obj)
+   #bpy.data.collections['VIEW.001'].objects.link(obj)
+   bpy.context.scene.collection.objects.unlink(obj)
+   bpy.context.active_object.data.materials.append(GLASS)
+   #-----------------------------------------------------------------
+   font_curve = bpy.data.curves.new(type="FONT",name=OBJ_N)
+   font_curve.body = "VIEW :"+OBJ_N
+   font_obj = bpy.data.objects.new("TEXT_" + OBJ_N, font_curve)
+   #bpy.ops.transform.resize(value=( ROW_C/50 , MEM_D/10 , 50) )
+   font_obj.location = (1 , CICLO_V, OBJ_HGT + 1)
+   bpy.data.collections['TEXT'].objects.link(font_obj)
+   #bpy.context.scene.collection.objects.unlink(font_obj)
+   CICLO_V = CICLO_V -10
+ #-- GESTIONE DB MULTIPLI -- -- -- -- -- -- -- -- --
+ DB_FLR  = (DB_FLR*2.2) #preparing for new BD Floor
+ #-----------------------------------------------------------------
+ print('DONE \n')
+ QUERY_5 = str("DROP VIEW TEST_DB_PAYROLL.")+ DB_NAME + str(";")
+ session.execute(QUERY_5)
+ print( DB_NAME + 'DROPED \n')
